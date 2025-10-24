558dd3598b5ce643d77b45bd344b47bb677805c8
Rizky Albani <75643913+rzkyalbani@users.noreply.github.com>
2025-10-24 21:32:38 +0700

    Implement image zipping after saving
    Added functionality to zip images after saving them.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-import json, base64, os
+import json, base64, os, zipfile
+from google.colab import files
 
-with open("hasil.ipynb", "r", encoding="utf-8") as f:
+with open("/content/predict_student_with_XAI.ipynb", "r", encoding="utf-8") as f:
     nb = json.load(f)
 
 os.makedirs("images", exist_ok=True)
@@ -15,3 +16,11 @@ for cell in nb["cells"]:
                 count += 1
 
 print(f"Disimpan {count} gambar di folder images/")
+
+zip_filename = "images.zip"
+with zipfile.ZipFile(zip_filename, "w", zipfile.ZIP_DEFLATED) as zipf:
+    for root, _, files_in_dir in os.walk("images"):
+        for file in files_in_dir:
+            zipf.write(os.path.join(root, file), arcname=file)
+
+print(f"File zip selesai dibuat: {zip_filename}")
